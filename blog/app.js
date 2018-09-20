bf332cdc7d680edd1dbeb83a68bea6011276769f
lixu <lixu19960317@gmail.com>
2018-09-20 23:46:48 +0800

    feat(blog)

---
+++ blog/app.js
@@ -0,0 +1,71 @@
+const render = require('./lib/render');
+const logger = require('koa-logger');
+const router = require('koa-router')();
+const koaBody = require('koa-body');
+
+const Koa = require('koa');
+const app = module.exports = new Koa();
+
+// databases
+const posts = [];
+
+// middware
+app.use(logger());
+
+app.use(render);
+
+app.use(koaBody());
+
+
+// route definitions 
+router.get('/', list)
+    .get('/post/new', add)
+    .get('/post/:id', show)
+    .post('/post', create)
+
+app.use(router.routes());
+
+
+// post listing 
+async function list(ctx) {
+    await ctx.render('list', {posts: posts})
+}
+
+// show creation form
+async function add(ctx) {
+    // await ctx.render('list', {posts: posts})
+    await ctx.render('new');
+}
+
+
+// show post :id 
+async function show(ctx) {
+    const id = ctx.params.id;
+    const post = posts[id];
+    if (!post) {
+        ctx.throw(404, 'invalid post id')
+    }
+    await ctx.render('show', {post: post})
+}
+
+
+// create a post
+async function create(ctx) {
+    const post = ctx.request.body;
+    const id = posts.push(post) - 1;
+    post.create_at = new Date();
+    post.id = id;
+    ctx.redirect('/')
+}
+
+
+app.listen(3000)
+
+
+
+
+
+
+
+
+
